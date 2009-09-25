ecc192e2e5d72090bb35b142c6dc4639ec2542f0
Dave Airlie <airlied@linux.ie>
2009-09-26 06:03:51 +1000

    radeon: fix zaphod

--- src/radeon_driver.c
+++ src/radeon_driver.c
@@ -3727,6 +3727,8 @@ Bool RADEONScreenInit(int scrnIndex, ScreenPtr pScreen,
     memset(info->FB + pScrn->fbOffset, 0,
            pScrn->virtualY * pScrn->displayWidth * info->CurrentLayout.pixel_bytes);
 
+    pScrn->pScreen = pScreen;
+
     /* set the modes with desired rotation, etc. */
     if (!xf86SetDesiredModes (pScrn))
 	return FALSE;
