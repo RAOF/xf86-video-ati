RAOF/xf86-video-ati

commit a9c135809945fb372ce390f06918b9fd8e455b3f

Xv: fix textured video alignment
Signed-off-by: Alex Deucher <alexdeucher@gmail.com>

src/radeon_textured_video.c
@@ -284,10 +284,17 @@ RADEONPutImageTextured(ScrnInfoPtr pScrn,
 	    pPriv->bicubic_enabled = FALSE;
     }
 
-    if (info->ChipFamily >= CHIP_FAMILY_R600)
-	hw_align = 256;
+#ifdef XF86DRM_MODE
+    if (info->cs)
+	hw_align = drmmode_get_base_align(pScrn, 2, 0);
     else
-	hw_align = 64;
+#endif
+    {
+	if (info->ChipFamily >= CHIP_FAMILY_R600)
+	    hw_align = 256;
+	else
+	    hw_align = 64;
+    }
 
     switch(id) {
     case FOURCC_YV12:

src/radeon_legacy_memory.c
@@ -29,7 +29,7 @@ radeon_legacy_allocate_memory(ScrnInfoPtr pScrn,
 	if (*mem_struct)
 		radeon_legacy_free_memory(pScrn, *mem_struct);
 
-	video_bo = radeon_bo_open(info->bufmgr, 0, size, RADEON_GPU_PAGE_SIZE, domain, 0);
+	video_bo = radeon_bo_open(info->bufmgr, 0, size, align, domain, 0);
 
 	*mem_struct = video_bo;
 

commit 4d7e1498f7d9eb50e2eddabca193fc27bde24f0e

fix Xv on pre-r6xx asics
broken by:
a9c1358099:"Xv: fix textured video alignment"

Reported by:
Artur Skawina <art.08.09@gmail.com>

Signed-off-by: Alex Deucher <alexdeucher@gmail.com>

src/radeon_textured_video.c
@@ -284,9 +284,12 @@ RADEONPutImageTextured(ScrnInfoPtr pScrn,
     }
 
 #ifdef XF86DRM_MODE
-    if (info->cs)
-	pPriv->hw_align = drmmode_get_base_align(pScrn, 2, 0);
-    else
+    if (info->cs) {
+	if (info->ChipFamily >= CHIP_FAMILY_R600)
+	    pPriv->hw_align = drmmode_get_base_align(pScrn, 2, 0);
+	else
+	    pPriv->hw_align = 64;
+    } else
 #endif
     {
 	if (info->ChipFamily >= CHIP_FAMILY_R600)